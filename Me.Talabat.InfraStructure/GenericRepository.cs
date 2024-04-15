fe3e28fbade60fb668999dc5d6cbf6973c62e7a5
Mahmoud Khamis Ahmed <152819907+Mahmoud-Khamis-Claro@users.noreply.github.com>
2024-04-15 12:28:30 +0200

    Generic Repository

---
+++ Me.Talabat.InfraStructure/GenericRepository.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Me.Talabat.InfraStructure.Data;
+using Me.Talabt.Core.Entities;
+using Me.Talabt.Core.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace Me.Talabat.InfraStructure
+{
+	public class GenericRepository<T> : IGenericRepository<T> where T : BaseEntity
+	{
+		private readonly ApplicationDbContext _dbContext;
+
+		public GenericRepository(ApplicationDbContext dbContext)
+		{
+			_dbContext = dbContext;
+		}
+
+		public async Task<IEnumerable<T>> GetAllAsync()
+		{
+			return await _dbContext.Set<T>().ToListAsync<T>();
+		}
+
+		public async Task<T?> GetAsync(int id)
+		{
+			return await _dbContext.Set<T>().Where(P => P.Id == id).FirstOrDefaultAsync();
+		}
+	}
+}
---
+++ Me.Talabt.Core/Repositories.Contract/IGenericRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Me.Talabt.Core.Entities;
+
+namespace Me.Talabt.Core.Repositories
+{
+	public interface IGenericRepository<T> where T : BaseEntity
+	{
+		public Task<T?> GetAsync(int id);
+
+		public Task<IEnumerable<T>> GetAllAsync();
+    }
+}
