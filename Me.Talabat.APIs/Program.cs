516ee0b728c6353e831a84d16df9d2d3b44d9533
Mahmoud Khamis Ahmed <152819907+Mahmoud-Khamis-Claro@users.noreply.github.com>
2024-04-15 12:12:24 +0200

    data seeding

--- Me.Talabat.APIs/Program.cs
+++ Me.Talabat.APIs/Program.cs
@@ -1,5 +1,8 @@
 
+using System.IdentityModel.Tokens.Jwt;
+using System.Text.Json;
 using Me.Talabat.InfraStructure.Data;
+using Me.Talabt.Core.Entities;
 using Microsoft.EntityFrameworkCore;
 
 namespace Me.Talabat.APIs
@@ -22,10 +25,19 @@ namespace Me.Talabat.APIs
 			#endregion
 
 			
-			var app = webApplicationBuilder.Build();
+			using var app = webApplicationBuilder.Build();
 			var scope = app.Services.CreateScope();
 			var dbContext = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
-			dbContext.Database.Migrate();
+			var  LoggerFactory = scope.ServiceProvider.GetRequiredService<ILoggerFactory>();
+			try
+			{
+				dbContext.Database.Migrate();
+
+			}catch (Exception ex)
+			{
+				var logger = LoggerFactory.CreateLogger<Program>();
+				logger.LogError(ex.Message, "an error occured while updating database");
+			}
 			#region Cinfigurations
 			// Configure the HTTP request pipeline.
 			if (app.Environment.IsDevelopment())
@@ -39,9 +51,42 @@ namespace Me.Talabat.APIs
 			app.UseAuthorization();
 
 
-			app.MapControllers(); 
+			app.MapControllers();
 			#endregion
 
+			var productsFile = File.ReadAllText("../Me.Talabat.InfraStructure/Data/Data Seeding JSON Files/products.json");
+			var products = JsonSerializer.Deserialize<List<Product>>(productsFile);
+
+			if (products is not null && dbContext.Set<Product>().Count() == 0)
+			{
+				foreach (var item in products)
+				{
+					dbContext.Set<Product>().Add(item);
+				}
+			}
+
+			var brandsFile = File.ReadAllText("../Me.Talabat.InfraStructure/Data/Data Seeding JSON Files/brands.json");
+			var brands = JsonSerializer.Deserialize<List<ProductBrand>>(brandsFile);
+
+			if (brands is not null && dbContext.Set<ProductBrand>().Count() == 0)
+			{
+				foreach (var item in brands)
+				{
+					dbContext.Set<ProductBrand>().Add(item);
+				}
+			}
+
+			var categoriesFile = File.ReadAllText("../Me.Talabat.InfraStructure/Data/Data Seeding JSON Files/categories.json");
+			var categories = JsonSerializer.Deserialize<List<ProductCategory>>(categoriesFile);
+
+			if (categories is not null && dbContext.Set<ProductCategory>().Count() == 0)
+			{
+				foreach (var item in categories)
+				{
+					dbContext.Set<ProductCategory>().Add(item);
+				}
+			}
+			dbContext.SaveChanges();
 			app.Run();
 		}
 	}
