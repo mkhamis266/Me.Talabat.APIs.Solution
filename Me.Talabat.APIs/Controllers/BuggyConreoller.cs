2e08e89d480b759ecc65ad85c9b67cdf7018ec74
Mahmoud Khamis Ahmed <152819907+Mahmoud-Khamis-Claro@users.noreply.github.com>
2024-04-17 13:53:18 +0200

    BuggyController

---
+++ Me.Talabat.APIs/Controllers/BuggyConreoller.cs
@@ -0,0 +1,50 @@
+﻿using Me.Talabat.InfraStructure.Data;
+using Me.Talabt.Core.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Me.Talabat.APIs.Controllers
+{
+	public class BuggyController : BaseApiController
+	{
+		private readonly ApplicationDbContext _dbcontext;
+
+		public BuggyController(ApplicationDbContext dbcontext)
+		{
+			_dbcontext = dbcontext;
+		}
+
+		[HttpGet("notfound")]
+		public ActionResult GetNotFoundRequest()
+		{
+			var Product = _dbcontext.Set<Product>().Find(100);
+			if (Product is not null)
+				return Ok(Product);
+
+			return NotFound();
+		}
+
+		[HttpGet("servererror")]
+		public ActionResult GetServerError()
+		{
+			var Product = _dbcontext.Set<Product>().Find(100);
+			var ProductToReturn = Product.ToString();
+			return Ok(ProductToReturn);
+		}
+		[HttpGet("badrequest")]
+		public ActionResult GetBaddRequest()
+		{
+			return BadRequest();
+		}
+		[HttpGet("badrequest/{id}")] //badrequest/five
+		public ActionResult GetBaddRequest(int id)
+		{
+			return Ok();
+		}
+		[HttpGet("unauthorized")] //badrequest/five
+		public ActionResult GetUnauthorized(int id)
+		{
+			return Unauthorized();
+		}
+	}
+}
